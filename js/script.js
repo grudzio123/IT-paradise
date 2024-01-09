80b9afc6b7b94381589467425fc4833e8cffb8de
grudzio123 <dave_grudzinski@wp.pl>
2024-01-09 18:08:37 +0100

    header media fixed

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const nav = document.querySelector('.nav__hidden')
+const navBtn = document.querySelector('.burger-btn')
+const allNavItems = document.querySelectorAll('.nav__item')
+
+const showNav = () => {
+	nav.classList.toggle('nav--active')
+
+	allNavItems.forEach(item => {
+		item.addEventListener('click', () => {
+			nav.classList.remove('nav--active')
+		})
+	})
+
+	PlayNavItemsAnimation();
+}
+
+const PlayNavItemsAnimation = () => {
+	let delayTime = 0;
+
+	allNavItems.forEach(item => {
+		item.classList.toggle('nav-items-animation')
+		item.style.animationDelay = '.' + delayTime + 's';
+		delayTime++; 
+	})
+}
+
+navBtn.addEventListener('click', showNav)
